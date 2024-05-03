043e611973b8ca871828025d90e9039e3470e824
David Udensi <davidudensi@gmail.com>
2024-05-03 22:53:09 +0100

    updated current checkout service unit tests

--- CheckoutKata.Tests/Systems/Services/TestCheckoutService.cs
+++ CheckoutKata.Tests/Systems/Services/TestCheckoutService.cs
@@ -1,4 +1,5 @@
 using CheckoutKata.Core.Services;
+using CheckoutKata.Tests.Fixtures;
 using FluentAssertions;
 using Microsoft.Extensions.Logging;
 using Moq;
@@ -22,46 +23,43 @@ namespace CheckoutKata.Tests.Systems.Services
         [Fact]
         public void Constructor_IfRulesAreWrongDtoFormatShould_ThrowException()
         {
-            var rules_string = File.ReadAllText("rules-wrong-format.json");
-            Assert.Throws<Exception>(() => new CheckoutService(rules_string, logger.Object));
+            Assert.Throws<Exception>(() => new CheckoutService(PricingRuleFixture.RULES_WRONG_FORMAT, logger.Object));
         }
 
         [Fact]
         public void Constructor_ShouldSkipNullEmptySKUs_CreateOneNotTwo()
         {
-            var rules_string = File.ReadAllText("rules-two.json");
-            var sut = new CheckoutService(rules_string, logger.Object);
+            var sut = new CheckoutService(PricingRuleFixture.RULES_WITH_EMPTY_SKU, logger.Object);
             sut.Rules.Should().HaveCount(1);
         }
 
         [Fact]
         public void Constructor_ShouldReplaceOldRule_WithNewerRule_UnitPrice_ShouldBe75()
         {
-            var rules_string = File.ReadAllText("rules-replace.json");
-            var sut = new CheckoutService(rules_string, logger.Object);
+            var sut = new CheckoutService(PricingRuleFixture.RULES_WITH_REPEATED_SKUS, logger.Object);
             var rule = sut.Rules["A"];
             Assert.Equal(75, rule?.UnitPrice);
         }
 
         [Theory]
-        [InlineData("ABCD", 4)]
-        [InlineData("AACD", 3)]
-        [InlineData("AAAA", 1)]
-        [InlineData("AABB", 2)]
-        public void Scan_WhenCalled_UpdatesCart_WithSKUs_CountShouldBeResult(string items, int result)
+        [InlineData(new string[] { "AAAA", "BBBB", "CCCC", "DDDD" }, 4)]
+        [InlineData(new string[] { "AAAA", "BBBB", "CCCC" }, 3)]
+        [InlineData(new string[] { "AAAA", "BBBB" }, 2)]
+        [InlineData(new string[] { "AAAA" }, 1)]
+        public void Scan_WhenCalled_UpdatesCart_WithSKUs_CountShouldBeResult(string[] items, int result)
         {
-            var rules_string = File.ReadAllText("rules.json");
-            var sut = new CheckoutService(rules_string, logger.Object);
-            sut.ClearCart();
-            sut.Scan(items);
+            var sut = new CheckoutService(PricingRuleFixture.RULES_PASSING, logger.Object);
+            foreach (string item in items)
+            {
+                sut.Scan(item);
+            }
             sut.CartItems.Should().HaveCount(result);
         }
 
         [Fact]
         public void Scan_WhenCalled_UpdatesCart_WithEmptySKUs_ShouldThrowNullReferenceException()
         {
-            var rules_string = File.ReadAllText("rules.json");
-            var sut = new CheckoutService(rules_string, logger.Object);
+            var sut = new CheckoutService(PricingRuleFixture.RULES_PASSING, logger.Object);
             sut.ClearCart();
             Assert.Throws<NullReferenceException>(() => sut.Scan(""));
         }
@@ -69,9 +67,8 @@ namespace CheckoutKata.Tests.Systems.Services
         [Fact]
         public void ClearCart_WhenCalled_ShouldSetCartItems_Equals0()
         {
-            var rules_string = File.ReadAllText("rules.json");
-            var sut = new CheckoutService(rules_string, logger.Object);
-            sut.Scan("ABCD");
+            var sut = new CheckoutService(PricingRuleFixture.RULES_PASSING, logger.Object);
+            sut.Scan("AAAA");
             sut.ClearCart();
             sut.CartItems.Should().HaveCount(0);
         }
@@ -79,38 +76,38 @@ namespace CheckoutKata.Tests.Systems.Services
         [Fact]
         public void GetTotalPrice_WhenCalled_ShouldReturn_Integer()
         {
-            var rules_string = File.ReadAllText("rules.json");
-            var sut = new CheckoutService(rules_string, logger.Object);
+            var sut = new CheckoutService(PricingRuleFixture.RULES_PASSING, logger.Object);
             sut.Scan("A");
             var result = sut.GetTotalPrice();
             Assert.True(result.GetType() == typeof(int));
         }
 
         [Theory]
-        [InlineData("A", 50)]
-        [InlineData("AA", 100)]
-        [InlineData("AAA", 130)]
-        [InlineData("AAAA", 180)]
-        [InlineData("AAAAA", 230)]
-        [InlineData("AAAAAA", 260)]
-        public void GetTotalPrice_WhenCalled_ShouldCalculate_Total(string items, int expected)
+        [InlineData(new string[] { "AAAA" }, 50)]
+        [InlineData(new string[] { "AAAA", "AAAA" }, 100)]
+        [InlineData(new string[] { "AAAA", "AAAA", "AAAA" }, 130)]
+        [InlineData(new string[] { "AAAA", "AAAA", "AAAA", "AAAA" }, 180)]
+        [InlineData(new string[] { "AAAA", "AAAA", "AAAA", "AAAA", "AAAA" }, 230)]
+        [InlineData(new string[] { "AAAA", "AAAA", "AAAA", "AAAA", "AAAA", "AAAA" }, 260)]
+        public void GetTotalPrice_WhenCalled_ShouldCalculate_Total(string[] items, int expected)
         {
-            var rules_string = File.ReadAllText("rules.json");
-            var sut = new CheckoutService(rules_string, logger.Object);
-            sut.Scan(items);
+            var sut = new CheckoutService(PricingRuleFixture.RULES_PASSING, logger.Object);
+            foreach (string item in items)
+            {
+                sut.Scan(item);
+            }
             var total = sut.GetTotalPrice();
             total.Should().Be(expected);
         }
 
         [Theory]
-        [InlineData("ABCD")]
-        [InlineData("AACD")]
         [InlineData("AAAA")]
-        [InlineData("AABB")]
+        [InlineData("BBBB")]
+        [InlineData("CCCC")]
+        [InlineData("DDDD")]
         public void GetTotalPrice_WhenCartItems_IsCleared_ShouldReturn0(string items)
         {
-            var rules_string = File.ReadAllText("rules.json");
-            var sut = new CheckoutService(rules_string, logger.Object);
+            var sut = new CheckoutService(PricingRuleFixture.RULES_PASSING, logger.Object);
             sut.Scan(items);
             sut.ClearCart();
             var total = sut.GetTotalPrice();
@@ -118,14 +115,13 @@ namespace CheckoutKata.Tests.Systems.Services
         }
 
         [Theory]
-        [InlineData("E")]
-        [InlineData("P")]
-        [InlineData("Z")]
-        [InlineData("H")]
+        [InlineData("ABCD")]
+        [InlineData("PUYT")]
+        [InlineData("ZUIO")]
+        [InlineData("HASC")]
         public void Scan_WhenSKUDoesNotExitInRules_ShouldSkipSKU_CartShouldBeEmpty(string item)
         {
-            var rules_string = File.ReadAllText("rules.json");  // Rules for A, B, C and D
-            var sut = new CheckoutService(rules_string, logger.Object);
+            var sut = new CheckoutService(PricingRuleFixture.RULES_PASSING, logger.Object);
             sut.Scan(item);
             sut.CartItems.Should().HaveCount(0);
         }
