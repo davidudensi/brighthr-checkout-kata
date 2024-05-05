6d1720e841089454a592066130e6751edb2f269d
David Udensi <davidudensi@gmail.com>
2024-05-06 00:21:24 +0100

    added fixture for variable lenght sku

--- CheckoutKata.Tests/Fixtures/PricingRuleFixture.cs
+++ CheckoutKata.Tests/Fixtures/PricingRuleFixture.cs
@@ -9,6 +9,7 @@ namespace CheckoutKata.Tests.Fixtures
         public static PricingRule GetRegularPricingRule() => new("A", 50);
         public static PricingRule GetSpecialPricingRule() => new("A", 50, specialPrice);
         public const string RULES_PASSING = "[{\"SKU\": \"AAAA\",\"UnitPrice\": 50,\"SpecialPrice\": {\"Units\": 3,\"Price\": 130}},{\"SKU\": \"BBBB\",\"UnitPrice\": 30,\"SpecialPrice\": {\"Units\": 2,\"Price\": 45}},{\"SKU\": \"CCCC\",\"UnitPrice\": 20,\"SpecialPrice\": null},{\"SKU\": \"DDDD\",\"UnitPrice\": 15,\"SpecialPrice\": null}]";
+        public const string RULES_VARIABLE_LENGTH_SKUS = "[{\"SKU\": \"A\",\"UnitPrice\": 50,\"SpecialPrice\": {\"Units\": 3,\"Price\": 130}},{\"SKU\": \"AB\",\"UnitPrice\": 30,\"SpecialPrice\": {\"Units\": 2,\"Price\": 45}},{\"SKU\": \"ABC\",\"UnitPrice\": 20,\"SpecialPrice\": null},{\"SKU\": \"ABCD\",\"UnitPrice\": 15,\"SpecialPrice\": null}]";
         public const string RULES_WRONG_FORMAT = "{\"SKU\": \"A\",\"UnitPrice\": 50,\"SpecialPrice\": {\"Units\": 3,\"Price\": 130}}";
         public const string RULES_WITH_EMPTY_SKU = "[{\"SKU\": \"A\",\"UnitPrice\": 50,\"SpecialPrice\": {\"Units\": 3,\"Price\": 130}},{\"SKU\": \"\",\"UnitPrice\": 30,\"SpecialPrice\": null}]";
         public const string RULES_WITH_REPEATED_SKUS = "[{\"SKU\": \"A\",\"UnitPrice\": 50,\"SpecialPrice\": {\"Units\": 3,\"Price\": 130}},{\"SKU\": \"A\",\"UnitPrice\": 75,\"SpecialPrice\": {\"Units\": 3,\"Price\": 200}}]";
